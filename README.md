e31769d68a6949c6f0af5a0f93b77c7a1f30c7e9
dannydk6 <dannydk6@gmail.com>
2020-04-20 11:50:11 -0400

    setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Dash on Flask Upgrades
+
 # Dash on flask with flask_login
 An example of a seamless integration of a Dash app into an existing Flask app based on the application factory pattern.
 
--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+load_dotenv()
 
-
+print(os.getenv('DATABASE_URL'))
 class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
